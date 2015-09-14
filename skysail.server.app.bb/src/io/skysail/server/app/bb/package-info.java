3ff0281652b8aaaf124f4bdbed96863973a38b09
Carsten Gräf <evandor@gmail.com>
2015-09-14 08:42:07 +0200

    new bndtools version

---
+++ skysail.server.app.bb/src/io/skysail/server/app/bb/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package io.skysail.server.app.bb;
+
+import org.osgi.annotation.versioning.Version;
---
+++ test/src/test/ExampleComponent.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class ExampleComponent {
+
+	// TODO: class provided by template
+
+}
