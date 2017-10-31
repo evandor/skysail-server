57a5165324970a855cae9c97967fa103800b7e18
Carsten Graef <evandor@gmail.com>
2017-10-31 10:02:13 +0100

    added akka http for auth

--- skysail.api/src/io/skysail/api/security/AuthenticationService.scala
+++ skysail.api/src/io/skysail/api/security/AuthenticationService.scala
@@ -1,8 +1,8 @@
 package io.skysail.api.security
 
-//import akka.http.scaladsl.server.Directives._
-//import akka.http.scaladsl.server.{ Directive1, PathMatcher, RequestContext, Route }
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{ Directive1, PathMatcher, RequestContext, Route }
 
 trait AuthenticationService {
-  //def directive(): Directive1[String]
+  def directive(): Directive1[String]
 }
