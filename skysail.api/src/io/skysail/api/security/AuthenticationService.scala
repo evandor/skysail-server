91a66b6c83a5733f3c5c1afb3e79f04db38e2dd5
Carsten Graef <evandor@gmail.com>
2017-10-31 08:47:02 +0100

    added new package to api

---
+++ skysail.api/src/io/skysail/api/security/AuthenticationService.scala
@@ -0,0 +1,8 @@
+package io.skysail.api.security
+
+//import akka.http.scaladsl.server.Directives._
+//import akka.http.scaladsl.server.{ Directive1, PathMatcher, RequestContext, Route }
+
+trait AuthenticationService {
+  //def directive(): Directive1[String]
+}
---
+++ skysail.api/src/io/skysail/api/security/AuthorizationService.scala
@@ -0,0 +1,5 @@
+package io.skysail.api.security
+
+trait AuthorizationService {
+  
+}
