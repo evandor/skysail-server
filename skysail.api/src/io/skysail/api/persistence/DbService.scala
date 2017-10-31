f69e2d02932bfcccef829e20b51e323825fd15ee
Carsten Graef <evandor@gmail.com>
2017-10-31 07:25:53 +0100

    added persistence package

---
+++ skysail.api/src/io/skysail/api/persistence/DbService.scala
@@ -0,0 +1,16 @@
+package io.skysail.api.persistence
+
+object DbService {
+  def tableNameFor(cls: Class[_]): String = cls.getName().replace(".", "_")
+}
+
+trait DbService {
+
+  def createWithSuperClass(superClass: String, vertices: String*)
+
+  def register(classes: Class[_]*)
+
+  def persist(entity: Any): String
+  
+  def findGraphs[T:Manifest](cls: Class[T], sql: String /*, Map<String, Object> params*/ ): List[T]
+}
