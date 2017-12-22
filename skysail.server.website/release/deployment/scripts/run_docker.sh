afa625ee0a025f633e617a485c45a985ead7941d
Carsten Gräf <evandor@gmail.com>
2017-12-22 10:40:59 +0100

    fixing logback path

--- skysail.server.website/release/deployment/scripts/run_docker.sh
+++ skysail.server.website/release/deployment/scripts/run_docker.sh
@@ -12,4 +12,9 @@ del_stopped(){
 
 ## Skysail Server Website
 del_stopped skysail-server-website
-docker run --name skysail-server-website -t --rm -p 9202:9202 -v /home/carsten/skysail/skysailconfigs/website/test:/home/skysail/products/demo/config -e CONFPATH=config evandor/skysail-server-website:latest
+docker run \
+    --name skysail-server-website \
+    -t --rm -p 9202:9202 \
+    -v /home/carsten/skysail/skysailconfigs/website/test:/home/skysail/products/demo/config \
+    -e CONFPATH=config \
+    evandor/skysail-server-website:latest
