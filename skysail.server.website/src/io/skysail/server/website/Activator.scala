05fad6ffc575d90c001db0ed56c0ad64ae32250f
Carsten Gräf <evandor@gmail.com>
2017-12-22 19:59:05 +0100

    trying to improve logging (jul)

--- skysail.server.website/src/io/skysail/server/website/Activator.scala
+++ skysail.server.website/src/io/skysail/server/website/Activator.scala
@@ -3,8 +3,12 @@ package io.skysail.server.website
 import akka.actor.ActorSystem
 import domino.DominoActivator
 import io.skysail.server.app.ApplicationProvider
+import org.slf4j.bridge.SLF4JBridgeHandler
 
 class Activator extends DominoActivator {
+
+  SLF4JBridgeHandler.removeHandlersForRootLogger
+  SLF4JBridgeHandler.install
 
   whenBundleActive {
     whenServicePresent[ActorSystem] { system =>
