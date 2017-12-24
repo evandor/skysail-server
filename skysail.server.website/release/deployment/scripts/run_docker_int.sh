9fbbea88aef286074c07ef310f7b69a946d03d48
Carsten Gräf <evandor@gmail.com>
2017-12-24 11:24:23 +0100

    port fix

--- skysail.server.website/release/deployment/scripts/run_docker_int.sh
+++ skysail.server.website/release/deployment/scripts/run_docker_int.sh
@@ -14,7 +14,7 @@ del_stopped(){
 del_stopped skysail-server-website-int
 docker run \
     --name skysail-server-website-int \
-    -t --rm -p 9202:9202 \
+    -t --rm -p 9201:9201 \
     -v /home/carsten/skysail/skysailconfigs/website/int:/home/skysail/products/demo/config \
     -e CONFPATH=config \
     evandor/skysail-server-website:latest
