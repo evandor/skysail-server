b75fe8de5f3e7a2c14ad2a3f962652caca5ed1ad
Carsten Gräf <evandor@gmail.com>
2018-01-13 09:19:53 +0100

    docker issues

--- skysail.server.website/release/deployment/scripts/run_docker_test.sh
+++ skysail.server.website/release/deployment/scripts/run_docker_test.sh
@@ -4,14 +4,19 @@ del_stopped(){
   local name=$1
   local state=$(docker inspect --format "{{.State.Running}}" $name 2>/dev/null)
 
+  echo "Stopping docker container '$1'"
+
   if [[ "$state" == "true" ]]; then
     docker stop $name
-    docker rm $name
+    //docker rm $name
   fi
 }
 
 ## Skysail Server Website
 del_stopped skysail-server-website-test
+
+echo "Starting docker container..."
+
 docker run \
     --name skysail-server-website-test \
     -t --rm -p 9202:9202 \
