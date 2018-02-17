f819f085c2ee466705c25ff62b72bc461eff7724
Carsten Gräf <evandor@gmail.com>
2018-02-17 08:51:42 +0100

    moved appboard app to skysail-apps repo

--- skysail.server.app.appboard/src/io/skysail/server/app/appboard/domain/application.scala
+++
@@ -1,7 +0,0 @@
-//package io.skysail.server.app.appboard.domain
-//
-//case class Application(name: String, context: String, description: String) //extends Entity[String]
-//
-//case class ApplicationList(
-//                            applications: List[Application]
-//                          )
