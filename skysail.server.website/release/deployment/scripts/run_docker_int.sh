52f104046024621a42399b6be73ef24d972b8acc
Carsten Gräf <evandor@gmail.com>
2018-03-03 11:36:49 +0100

    testing build pipeline

--- skysail.server.website/release/deployment/scripts/run_docker_int.sh
+++ skysail.server.website/release/deployment/scripts/run_docker_int.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-del_stopped(){
-  local name=$1
-  local state=$(docker inspect --format "{{.State.Running}}" $name 2>/dev/null)
+echo "deprecated, to be deleted!"
 
-  if [[ "$state" == "true" ]]; then
-    docker stop $name
-    docker rm $name
-  fi
-}
-
-## Skysail Server Website
-del_stopped skysail-server-website-int
-docker run \
-    --name skysail-server-website-int \
-    -t --rm -p 9201:9201 \
-    -v /home/carsten/skysail/skysailconfigs/website/int:/home/skysail/products/demo/config \
-    -e CONFPATH=config \
-    evandor/skysail-server-website-local:latest
+#del_stopped(){
+#  local name=$1
+#  local state=$(docker inspect --format "{{.State.Running}}" $name 2>/dev/null)
+#
+#  if [[ "$state" == "true" ]]; then
+#    docker stop $name
+#    docker rm $name
+#  fi
+#}
+#
+### Skysail Server Website
+#del_stopped skysail-server-website-int
+#docker run \
+#    --name skysail-server-website-int \
+#    -t --rm -p 9201:9201 \
+#    -v /home/carsten/skysail/skysailconfigs/website/int:/home/skysail/products/demo/config \
+#    -e CONFPATH=config \
+#    evandor/skysail-server-website-local:latest
--- skysail.server.website/release/deployment/scripts/run_docker_test.sh
+++ skysail.server.website/release/deployment/scripts/run_docker_test.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-del_stopped(){
-  local name=$1
-  local state=$(docker inspect --format "{{.State.Running}}" $name 2>/dev/null)
+echo "deprecated, to be deleted!"
 
-  echo "Stopping docker container '$1'"
-
-  if [[ "$state" == "true" ]]; then
-    echo "running docker stop $name"
-    docker stop $name
-    echo "running docker rm $name"
-    docker rm $name
-  fi
-}
-
-## Skysail Server Website
-del_stopped skysail-server-website-test
-
-sleep 5 
-
-echo "Starting docker container!..."
-
-docker run \
-    -d
-    -t
-    --name skysail-server-website-test \
-    --rm
-    -p 9202:9202 \
-    -v /home/carsten/skysail/skysailconfigs/website/test:/home/skysail/products/demo/config \
-    -e CONFPATH=config \
-    evandor/skysail-server-website-local:latest
+#del_stopped(){
+#  local name=$1
+#  local state=$(docker inspect --format "{{.State.Running}}" $name 2>/dev/null)
+#
+#  echo "Stopping docker container '$1'"
+#
+#  if [[ "$state" == "true" ]]; then
+#    echo "running docker stop $name"
+#    docker stop $name
+#    echo "running docker rm $name"
+#    docker rm $name
+#  fi
+#}
+#
+### Skysail Server Website
+#del_stopped skysail-server-website-test
+#
+#sleep 5
+#
+#echo "Starting docker container!..."
+#
+#docker run \
+#    -d
+#    -t
+#    --name skysail-server-website-test \
+#    --rm
+#    -p 9202:9202 \
+#    -v /home/carsten/skysail/skysailconfigs/website/test:/home/skysail/products/demo/config \
+#    -e CONFPATH=config \
+#    evandor/skysail-server-website-local:latest
