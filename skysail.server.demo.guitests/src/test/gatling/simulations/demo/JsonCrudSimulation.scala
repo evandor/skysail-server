c167f32cd0a2bec74aa1f2b07c4bd63da4c3f8ac
evandor <evandor@gmail.com>
2018-03-28 17:15:53 +0200

    playing with gatling

--- skysail.server.demo.guitests/src/test/gatling/simulations/demo/JsonCrudSimulation.scala
+++ skysail.server.demo.guitests/src/test/gatling/simulations/demo/JsonCrudSimulation.scala
@@ -2,9 +2,10 @@ package demo
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
 
-class CrudSimulation extends Simulation {
+class JsonCrudSimulation extends Simulation {
 
   object HomePage {
 
@@ -34,33 +35,22 @@ class CrudSimulation extends Simulation {
 
   val httpConf = http
     .baseURL(baseUrl)
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .acceptHeader("application/json")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .extraInfoExtractor(ExtraInfo => {
+      println("httpCode: " + ExtraInfo.response.statusCode.getOrElse(0) +
+        "\nResponse: " + ExtraInfo.response.body.string); List(ExtraInfo.response.statusCode, ExtraInfo.response.body.string)
+    })
+
 
   val headers_10 = Map("Content-Type" -> "application/x-www-form-urlencoded") // Note the headers specific to a given request
 
-  val scn = scenario("redirect from root page")
-    .exec(
-      http("root").get("/")
-        .check(status.is(200))
-        .check(currentLocationRegex(".*demo/v1"))
-    )
-    .pause(1)
-
-
-
-
-  //setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
-  //  .assertions(forAll.failedRequests.percent.lte(5))
-
-  val users = scenario("CRUD").exec(HomePage.homepage, Comment1Page.get, Comment1Page.post)
-  //val admins = scenario("Admins").exec(Search.search, Browse.browse, Edit.edit)
+  val users = scenario("JSON CRUD").exec(HomePage.homepage)
 
   setUp(
     users.inject(rampUsers(1) over (1 seconds))
-    //admins.inject(rampUsers(2) over (10 seconds))
   ).protocols(httpConf).assertions(forAll.failedRequests.percent.lte(1))
 }
